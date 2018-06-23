33600a9d8aa2a2d217300ad8ca3fe881da9e52d7
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-06-23 20:43:55 +0200

    MetricsStreamInterface extends ObservableInterface

--- src/MetricsStreamInterface.php
+++ src/MetricsStreamInterface.php
@@ -2,9 +2,10 @@
 
 namespace WyriHaximus\React\Inspector;
 
+use Rx\ObservableInterface;
 use Rx\Subject\Subject;
 
-interface MetricsStreamInterface
+interface MetricsStreamInterface extends ObservableInterface
 {
     /**
      * Any implementers of this interface should extend Subject and only call onNext with instances of Metric.
